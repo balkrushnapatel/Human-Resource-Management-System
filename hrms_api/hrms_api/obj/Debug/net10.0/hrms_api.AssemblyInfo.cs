929db8f13f74e665c34583262eaf2eca7bd3f051
Smit Maru <smitmaru1226@gmail.com>
2026-01-03 13:11:35 +0530

    api check out

--- hrms_api/hrms_api/obj/Debug/net10.0/hrms_api.AssemblyInfo.cs
+++ hrms_api/hrms_api/obj/Debug/net10.0/hrms_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hrms_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45b2b0785c0cfa7dde4be0adb04ecdbf8983500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd3d444a9b7bb525bbf3e44e04a9d50dc3ffa3ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("hrms_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hrms_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
