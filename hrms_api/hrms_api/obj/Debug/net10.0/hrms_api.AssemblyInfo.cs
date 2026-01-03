98e5a49344d25a3d9b7fd55874eb9c6e5d990337
Balkrushna Patel <patelbalkrushna406@gmail.com>
2026-01-03 12:59:21 +0530

    Some Code Added

--- hrms_api/hrms_api/obj/Debug/net10.0/hrms_api.AssemblyInfo.cs
+++ hrms_api/hrms_api/obj/Debug/net10.0/hrms_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hrms_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee1ed1f6a80ae98e1753e6cd83886a22876ba86e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45b2b0785c0cfa7dde4be0adb04ecdbf8983500")]
 [assembly: System.Reflection.AssemblyProductAttribute("hrms_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hrms_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
